8331019ea01c13adccbadc111be0acfc54a2110c
Stan <stgajda@gmail.com>
2023-10-20 11:35:19 +0200

    redux toolkit store working

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import { store } from './src/store'
+import { Provider } from 'react-redux'
+const ReduxApp = () => (
+    <Provider store={store}>
+      <App />
+    </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => ReduxApp);
---
+++ src/MovieList.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FlatList, View, Text, Image } from 'react-native';
+
+import Styles from './Styles';
+import { Movie } from './Model';
+
+type Props = {
+  data: Movie[];
+};
+  
+const MovieList: React.FC<Props> = ({ data }) => {
+  const renderItem = ({ item }: { item: Movie }) => (
+    <View style={Styles.listItem}>
+      <Image
+        source={{ uri: `https://image.tmdb.org/t/p/w200${item.poster_path}` }}
+        style={Styles.image}
+      />
+      <View style={Styles.rightItemContainer}>
+        <Text numberOfLines={2} ellipsizeMode="tail" style={Styles.title}>
+          {item.title}
+        </Text>
+        <View style={Styles.ratingContainer}>
+          <Text style={Styles.rating}>{item.vote_average} </Text>
+          <Text style={Styles.voteCount}>({item.vote_count})</Text>
+        </View>
+      </View>
+    </View>
+  );
+
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={({ id }) => id}
+      renderItem={renderItem}
+    />
+  );
+};
+
+export default MovieList;
