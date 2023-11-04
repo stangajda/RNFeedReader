3177d1c5ab1945760176d9bed6c594ec7558be38
Stan <stgajda@gmail.com>
2023-11-04 17:17:27 +0100

    prettier code improvement and import react added

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
  * @format
  */
 
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-import { store } from './src/store'
-import { Provider } from 'react-redux'
+import {store} from './src/store';
+import {Provider} from 'react-redux';
 const ReduxApp = () => (
-    <Provider store={store}>
-      <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 AppRegistry.registerComponent(appName, () => ReduxApp);
