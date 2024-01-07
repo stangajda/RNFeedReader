cb444e46043e9b70aa71dfa495a5094171e19fae
Stan <stgajda@gmail.com>
2024-01-07 17:13:53 +0100

    add failed response to unit test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -3,7 +3,7 @@ import React, {ReactNode} from 'react';
 import {it} from '@jest/globals';
 import {http, HttpResponse} from 'msw';
 import {setupServer} from 'msw/node';
-import {useGetMoviesQuery} from '@src/apiSlice';
+import {apiSlice, useGetMoviesQuery} from '@src/apiSlice';
 import {renderHook, waitFor} from '@testing-library/react-native';
 
 import {store} from '@src/store';
@@ -15,42 +15,89 @@ function wrapper({children}: {children: ReactNode}) {
   return <Provider store={store}>{children}</Provider>;
 }
 
-describe('useGetMoviesQuery', () => {
-  const endpointName = 'getMovies';
-  const server = setupServer(
-    http.get('*/trending/movie/day', ({}) => {
-      return HttpResponse.json(data);
-    }),
-  );
+const server = setupServer();
 
-  beforeAll(() => server.listen());
-  afterEach(() => server.resetHandlers());
-  afterAll(() => server.close());
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
-  it('renders hook', async () => {
-    const {result} = renderHook(() => useGetMoviesQuery({}), {
-      wrapper,
+describe('check movie list service', () => {
+  describe('when successful json data', () => {
+    const endpointName = 'getMovies';
+
+    beforeAll(() => {
+      server.use(
+        http.get('*/trending/movie/day', () => {
+          return HttpResponse.json(data, {status: 200});
+        }),
+      );
     });
 
-    expect(result.current).toMatchObject({
-      status: 'pending',
-      endpointName,
-      isLoading: true,
-      isSuccess: false,
-      isError: false,
-      isFetching: true,
+    it('it should get successful response match mapped object', async () => {
+      const {result} = renderHook(() => useGetMoviesQuery({}), {
+        wrapper,
+      });
+
+      expect(result.current).toMatchObject({
+        status: 'pending',
+        endpointName,
+        isLoading: true,
+        isSuccess: false,
+        isError: false,
+        isFetching: true,
+      });
+
+      await waitFor(() => expect(result.current.isSuccess).toBe(true));
+      expect(result.current).toMatchObject({
+        status: 'fulfilled',
+        endpointName,
+        data,
+        isLoading: false,
+        isSuccess: true,
+        isError: false,
+        currentData: data,
+        isFetching: false,
+      });
+    });
+  });
+
+  describe('when failure error code', () => {
+    const endpointName = 'getMovies';
+
+    beforeAll(() => {
+      server.use(
+        http.get('*/trending/movie/day', () => {
+          return HttpResponse.json({error: 'Not Authorized'}, {status: 404});
+        }),
+      );
     });
 
-    await waitFor(() => expect(result.current.isSuccess).toBe(true));
-    expect(result.current).toMatchObject({
-      status: 'fulfilled',
-      endpointName,
-      data,
-      isLoading: false,
-      isSuccess: true,
-      isError: false,
-      currentData: data,
-      isFetching: false,
+    it('it should get failed response', async () => {
+      store.dispatch(apiSlice.util.resetApiState());
+      const {result} = renderHook(() => useGetMoviesQuery({}), {
+        wrapper,
+      });
+
+      expect(result.current).toMatchObject({
+        status: 'pending',
+        endpointName,
+        isLoading: true,
+        isSuccess: false,
+        isError: false,
+        isFetching: true,
+      });
+
+      await waitFor(() => expect(result.current.isError).toBe(true));
+      expect(result.current).toMatchObject({
+        status: 'rejected',
+        endpointName,
+        data: undefined,
+        isLoading: false,
+        isSuccess: false,
+        isError: true,
+        currentData: undefined,
+        isFetching: false,
+      });
     });
   });
 });
