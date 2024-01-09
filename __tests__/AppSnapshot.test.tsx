291c6e3a14a0a378c733638de5617e8658c21437
Stan <stgajda@gmail.com>
2024-01-09 17:23:51 +0100

    improved mocked hook useGetMoviesQuery

--- __tests__/AppSnapshot.test.tsx
+++ __tests__/AppSnapshot.test.tsx
@@ -4,30 +4,34 @@ import renderer from 'react-test-renderer';
 import App from '@src/App';
 import {Provider} from 'react-redux';
 import {store} from '@src/store';
+import {useGetMoviesQuery} from '@src/apiSlice';
 
 function wrapper({children}: {children: ReactNode}) {
   return <Provider store={store}>{children}</Provider>;
 }
 
 jest.mock('@src/apiSlice', () => {
-  const dataResult = require('./StubMovieListResponseResult.json');
   return {
     apiSlice: {
       reducerPath: 'api',
       reducer: () => ({}),
       middleware: () => (next: any) => (action: any) => next(action),
     },
-    useGetMoviesQuery: jest.fn().mockReturnValueOnce({
+    useGetMoviesQuery: jest.fn(),
+  };
+});
+
+describe('App snapshot test', () => {
+  beforeAll(() => {
+    const dataResult = require('./StubMovieListResponseResult.json');
+    (useGetMoviesQuery as jest.Mock).mockReturnValueOnce({
       data: dataResult,
       isLoading: false,
       isSuccess: true,
       isError: false,
       error: null,
-    }),
-  };
-});
-
-describe('App snapshot test', () => {
+    });
+  });
   it('renders correctly', () => {
     const tree = renderer.create(wrapper({children: <App />})).toJSON();
     expect(tree).toMatchSnapshot();
