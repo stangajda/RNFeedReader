72cfa0aced4f01f282e50eddd6786b4c37b56342
Stan <stgajda@gmail.com>
2024-01-09 18:24:38 +0100

    added all snapshot tests to cover all cases 100% percent code coverage achieved!

--- __tests__/AppSnapshot.test.tsx
+++ __tests__/AppSnapshot.test.tsx
@@ -19,19 +19,79 @@ jest.mock('@src/apiSlice', () => {
   };
 });
 
-describe('App snapshot test', () => {
-  beforeAll(() => {
-    const dataResult = require('./StubMovieListResponseResult.json');
-    (useGetMoviesQuery as jest.Mock).mockReturnValueOnce({
-      data: dataResult,
-      isLoading: false,
-      isSuccess: true,
-      isError: false,
-      error: null,
+describe('check movies list view to match recorded snapshot', () => {
+  describe('when movies list is loaded', () => {
+    beforeAll(() => {
+      const dataResult = require('./StubMovieListResponseResult.json');
+      (useGetMoviesQuery as jest.Mock).mockReturnValueOnce({
+        data: dataResult,
+        isLoading: false,
+        isSuccess: true,
+        isError: false,
+        error: null,
+      });
+    });
+    it('it should match movie list loaded image json', () => {
+      const tree = renderer.create(wrapper({children: <App />})).toJSON();
+      expect(tree).toMatchSnapshot();
     });
   });
-  it('renders correctly', () => {
-    const tree = renderer.create(wrapper({children: <App />})).toJSON();
-    expect(tree).toMatchSnapshot();
+  describe('when movies list is loading', () => {
+    beforeAll(() => {
+      (useGetMoviesQuery as jest.Mock).mockReturnValueOnce({
+        isLoading: true,
+        isSuccess: false,
+        isError: false,
+        error: null,
+      });
+    });
+    it('it should match movie list loading image json', () => {
+      const tree = renderer.create(wrapper({children: <App />})).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+  describe('when movies list is error', () => {
+    beforeAll(() => {
+      (useGetMoviesQuery as jest.Mock).mockReturnValueOnce({
+        isLoading: false,
+        isSuccess: false,
+        isError: true,
+        error: 'stub error message',
+      });
+    });
+    it('it should match movie list error image json', () => {
+      const tree = renderer.create(wrapper({children: <App />})).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+  describe('when movies list is empty', () => {
+    beforeAll(() => {
+      (useGetMoviesQuery as jest.Mock).mockReturnValueOnce({
+        data: [],
+        isLoading: false,
+        isSuccess: true,
+        isError: false,
+        error: null,
+      });
+    });
+    it('it should match movie list empty image json', () => {
+      const tree = renderer.create(wrapper({children: <App />})).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+  describe('when loading success and error at the same time return false', () => {
+    beforeAll(() => {
+      (useGetMoviesQuery as jest.Mock).mockReturnValueOnce({
+        data: null,
+        isLoading: false,
+        isSuccess: false,
+        isError: false,
+        error: null,
+      });
+    });
+    it('it should match movie list empty image json', () => {
+      const tree = renderer.create(wrapper({children: <App />})).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
   });
 });
