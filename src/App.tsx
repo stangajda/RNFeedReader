f95c258d9e48ac00144a44f99a770bf591973ad4
Stan <stgajda@gmail.com>
2024-02-05 22:23:10 +0100

    removed not needed code

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,14 @@ import React from 'react';
 import {ActivityIndicator, SafeAreaView, View, Text} from 'react-native';
 import Styles from './Styles';
 import MovieList from './MovieList';
-//import {MoviesContext} from './MoviesProvider';
 import {useInjection} from './MoviesProvider';
 
 import {Movie} from './Model';
 import {IMoviesQueryResult} from './interfaces';
 
-// import {Injection} from './DIContainer';
-// import {useHook} from './useHook';
-
 import {TYPES} from './types';
 
 function App(): React.JSX.Element {
-  //Injection.resolver.register('com1', useGetMoviesQuery({}));
-  //const {useGetMoviesQuery} = useContext(MoviesContext);
-  // const injection = Injection.getInstance();
-  // const queryResult = useGetMoviesQuery({});
-  // injection.register<IMoviesQueryResult>(
-  //   'IMoviesQueryResult',
-  //   () => queryResult,
-  // );
-
-  //useHook();
-  //const injection = Injection.getInstance();
-  //const {data, isLoading, isSuccess, isError, error} =
-  //injection.resolve<IMoviesQueryResult>(TYPES.IMoviesQueryResult);
-
   const provider = useInjection<IMoviesQueryResult>(TYPES.IMoviesQueryResult);
   const {data, isLoading, isSuccess, isError, error} = provider;
   const movieList: Movie[] = data?.results || [];
--- src/MoviesProvider.tsx
+++ src/MoviesProvider.tsx
@@ -1,50 +1,15 @@
 import React from 'react';
 import {IInjection} from './DIContainer';
-// import {TYPES} from './types';
-// import {IMoviesQueryResult} from './interfaces';
 import {useHook} from './useHook';
-//import {createContext} from 'react';
-
-// interface IInjectionContext {
-//   container: () => IInjection;
-// }
 
 const InjectionContext = React.createContext<{
   container: IInjection | null;
 }>({container: null});
 
-// export const MoviesContext = createContext<IMoviesQueryContext>({
-//   useGetMoviesQuery: () => ({
-//     data: undefined,
-//     isLoading: false,
-//     isSuccess: false,
-//     isError: false,
-//     error: undefined,
-//   }),
-// });
-
 type Props = {
   container: IInjection;
   children: React.ReactNode;
 };
-
-// interface InjectionProviderProps {
-//   children: React.ReactNode;
-// }
-
-// export function MoviesProvider({children}: InjectionProviderProps) {
-//   useHook();
-//   const mainContainerInstance = Injection.getInstance();
-//   const queryResult = mainContainerInstance.resolve<IMoviesQueryResult>(
-//     TYPES.IMoviesQueryResult,
-//   );
-
-//   return (
-//     <InjectionContext.Provider value={{container: () => Injection}}>
-//       {children}
-//     </InjectionContext.Provider>
-//   );
-// }
 
 export const MoviesProvider: React.FC<Props> = props => {
   useHook();
@@ -60,12 +25,5 @@ export function useInjection<T>(identifier: symbol) {
   if (!container) {
     throw new Error();
   }
-  //console.log('container', container);
   return container.resolve<T>(identifier);
 }
-
-// export function useInjection<T>(identifier: interfaces.ServiceIdentifier<T>) {
-//   const { container } = useContext(InversifyContext);
-//   if (!container) { throw new Error(); }
-//   return container.get<T>(identifier);
-// };
