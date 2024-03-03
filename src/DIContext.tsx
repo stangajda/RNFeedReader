d2a333bca3a23e48b450c110dc8e3ec34661d4e2
Stan <stgajda@gmail.com>
2024-03-03 23:46:22 +0100

    DIContext in typescript

---
+++ src/DIContext.tsx
@@ -0,0 +1,36 @@
+import React, {createContext, useContext, memo, ReactNode} from 'react';
+import {IMoviesQueryResult} from './interfaces';
+
+interface IDependencies {
+  moviesQueryResult: IMoviesQueryResult;
+}
+
+const defaultDependencies = {} as IDependencies;
+const DependenciesContext = createContext<IDependencies>(defaultDependencies);
+
+const useDependencies = () => useContext(DependenciesContext);
+
+export const DIProvider: React.FC<{children: ReactNode} & IDependencies> = memo(
+  function DIProvider({children, ...customDependencies}) {
+    const upstreamDependencies = useDependencies();
+
+    const dependencies: IDependencies = {
+      ...upstreamDependencies,
+      ...customDependencies,
+    };
+
+    return (
+      <DependenciesContext.Provider value={dependencies}>
+        {children}
+      </DependenciesContext.Provider>
+    );
+  },
+);
+
+export const useDI = (dependencies: IDependencies): IDependencies => {
+  const upstreamDependencies = useDependencies();
+
+  return Object.keys(upstreamDependencies).length > 0
+    ? {...upstreamDependencies}
+    : dependencies;
+};
