3f568f1ad7f7cb2c6655603784c85a7b91436f2b
Stan <stgajda@gmail.com>
2024-03-04 19:46:18 +0100

    all snapshot tests updated

--- __tests__/AppSnapshot.test.tsx
+++ __tests__/AppSnapshot.test.tsx
@@ -18,20 +18,20 @@ jest.mock('@src/apiSlice', () => {
   };
 });
 
-const mockUseGetMoviesQuery =
-  useGetMoviesQuery as jest.Mock<IMoviesQueryResult>;
-
 describe('check movies list view to match recorded snapshot', () => {
+  let mockData: IMoviesQueryResult;
   describe('when movies list is loaded', () => {
-    it('it should match movie list loaded image json', () => {
+    beforeAll(() => {
       const dataResult = require('./StubMovieListResponseResult.json');
-      const mockData: IMoviesQueryResult = {
+      mockData = {
         data: dataResult,
         isLoading: false,
         isSuccess: true,
         isError: false,
       };
+    });
 
+    it('should match movie list loaded image json', () => {
       const tree = renderer
         .create(
           <DIProvider moviesQueryResult={mockData}>
@@ -45,54 +45,78 @@ describe('check movies list view to match recorded snapshot', () => {
 
   describe('when movies list is loading', () => {
     beforeAll(() => {
-      mockUseGetMoviesQuery.mockReturnValueOnce({
+      mockData = {
         isLoading: true,
         isSuccess: false,
         isError: false,
-      });
+      };
     });
     it('it should match movie list loading image json', () => {
-      const tree = renderer.create(<ReduxApp />).toJSON();
+      const tree = renderer
+        .create(
+          <DIProvider moviesQueryResult={mockData}>
+            <ReduxApp />
+          </DIProvider>,
+        )
+        .toJSON();
       expect(tree).toMatchSnapshot();
     });
   });
   describe('when movies list is error', () => {
     beforeAll(() => {
-      mockUseGetMoviesQuery.mockReturnValueOnce({
+      mockData = {
         isLoading: false,
         isSuccess: false,
         isError: true,
         error: new Error('stub error message'),
-      });
+      };
     });
-    it('it should match movie list error image json', () => {
-      const tree = renderer.create(<ReduxApp />).toJSON();
+    it('it should match movie list loading image json', () => {
+      const tree = renderer
+        .create(
+          <DIProvider moviesQueryResult={mockData}>
+            <ReduxApp />
+          </DIProvider>,
+        )
+        .toJSON();
       expect(tree).toMatchSnapshot();
     });
   });
   describe('when movies list is empty', () => {
     beforeAll(() => {
-      mockUseGetMoviesQuery.mockReturnValueOnce({
+      mockData = {
         isLoading: false,
         isSuccess: true,
         isError: false,
-      });
+      };
     });
     it('it should match movie list empty image json', () => {
-      const tree = renderer.create(<ReduxApp />).toJSON();
+      const tree = renderer
+        .create(
+          <DIProvider moviesQueryResult={mockData}>
+            <ReduxApp />
+          </DIProvider>,
+        )
+        .toJSON();
       expect(tree).toMatchSnapshot();
     });
   });
   describe('when loading success and error at the same time return false', () => {
     beforeAll(() => {
-      mockUseGetMoviesQuery.mockReturnValueOnce({
+      mockData = {
         isLoading: false,
         isSuccess: false,
         isError: false,
-      });
+      };
     });
     it('it should match movie list empty image json', () => {
-      const tree = renderer.create(<ReduxApp />).toJSON();
+      const tree = renderer
+        .create(
+          <DIProvider moviesQueryResult={mockData}>
+            <ReduxApp />
+          </DIProvider>,
+        )
+        .toJSON();
       expect(tree).toMatchSnapshot();
     });
   });
