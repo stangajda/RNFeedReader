7ffcd243ba81a7c2f226df242065fa9396ec2081
Stan <stgajda@gmail.com>
2024-03-06 20:55:07 +0100

    optimised movie list using useCallback and memo

--- src/MovieList.tsx
+++ src/MovieList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {FC, memo, useCallback} from 'react';
 import {MOVIE_API_CONFIG} from './config';
 
 import {FlatList, View, Text, Image} from 'react-native';
@@ -10,30 +10,33 @@ type Props = {
   movieList: Movie[];
 };
 
-const MovieList: React.FC<Props> = ({movieList}) => {
-  const renderItem = ({
-    item: {id, poster_path, title, vote_average, vote_count},
-  }: {
-    item: Movie;
-  }) => (
-    <View style={Styles.listItem} key={id}>
-      <Image
-        source={{uri: `${MOVIE_API_CONFIG.movieImageUrl()}${poster_path}`}}
-        style={Styles.image}
-      />
-      <View style={Styles.rightItemContainer}>
-        <Text numberOfLines={2} ellipsizeMode="tail" style={Styles.title}>
-          {title}
-        </Text>
-        <View style={Styles.ratingContainer}>
-          <Text style={Styles.rating}>{vote_average} </Text>
-          <Text style={Styles.voteCount}>({vote_count})</Text>
+const MovieList: FC<Props> = ({movieList}) => {
+  const renderItem = useCallback(
+    ({
+      item: {id, poster_path, title, vote_average, vote_count},
+    }: {
+      item: Movie;
+    }) => (
+      <View style={Styles.listItem} key={id}>
+        <Image
+          source={{uri: `${MOVIE_API_CONFIG.movieImageUrl()}${poster_path}`}}
+          style={Styles.image}
+        />
+        <View style={Styles.rightItemContainer}>
+          <Text numberOfLines={2} ellipsizeMode="tail" style={Styles.title}>
+            {title}
+          </Text>
+          <View style={Styles.ratingContainer}>
+            <Text style={Styles.rating}>{vote_average} </Text>
+            <Text style={Styles.voteCount}>({vote_count})</Text>
+          </View>
         </View>
       </View>
-    </View>
+    ),
+    [],
   );
 
   return <FlatList data={movieList} renderItem={renderItem} />;
 };
 
-export default MovieList;
+export default memo(MovieList);
