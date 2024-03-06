abd785de943d982fdf1f8acef7fae7df28d86184
Stan <stgajda@gmail.com>
2024-03-06 21:27:41 +0100

    movie content component added

---
+++ src/MovieContent.tsx
@@ -0,0 +1,25 @@
+// MovieTitleAndRating.tsx
+import React, {memo} from 'react';
+import {View, Text} from 'react-native';
+import {Movie} from './model';
+import Styles from './styles';
+
+interface Props {
+  movie: Movie;
+}
+
+const MovieContent: React.FC<Props> = ({
+  movie: {title, vote_average, vote_count},
+}) => (
+  <View>
+    <Text numberOfLines={2} ellipsizeMode="tail" style={Styles.title}>
+      {title}
+    </Text>
+    <View style={Styles.ratingContainer}>
+      <Text style={Styles.rating}>{vote_average} </Text>
+      <Text style={Styles.voteCount}>({vote_count})</Text>
+    </View>
+  </View>
+);
+
+export default memo(MovieContent);
--- src/MovieList.tsx
+++ src/MovieList.tsx
@@ -1,35 +1,32 @@
 import React, {FC, memo, useCallback} from 'react';
 import {MOVIE_API_CONFIG} from './config';
 
-import {FlatList, View, Text, Image} from 'react-native';
+import {FlatList, View, Image} from 'react-native';
 
 import Styles from './styles';
 import {Movie} from './model';
+import MovieContent from './MovieContent';
 
 type Props = {
   movieList: Movie[];
 };
 
+type MovieItem = {
+  item: Movie;
+};
+
 const MovieList: FC<Props> = ({movieList}) => {
   const renderItem = useCallback(
-    ({
-      item: {id, poster_path, title, vote_average, vote_count},
-    }: {
-      item: Movie;
-    }) => (
-      <View style={Styles.listItem} key={id}>
+    ({item}: MovieItem) => (
+      <View style={Styles.listItem} key={item.id}>
         <Image
-          source={{uri: `${MOVIE_API_CONFIG.movieImageUrl()}${poster_path}`}}
+          source={{
+            uri: `${MOVIE_API_CONFIG.movieImageUrl()}${item.poster_path}`,
+          }}
           style={Styles.image}
         />
         <View style={Styles.rightItemContainer}>
-          <Text numberOfLines={2} ellipsizeMode="tail" style={Styles.title}>
-            {title}
-          </Text>
-          <View style={Styles.ratingContainer}>
-            <Text style={Styles.rating}>{vote_average} </Text>
-            <Text style={Styles.voteCount}>({vote_count})</Text>
-          </View>
+          <MovieContent movie={item} />
         </View>
       </View>
     ),
